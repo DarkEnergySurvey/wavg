ad4bef3a79ac84a6e1910345bcdbf4528cbf285f
Felipe Menanteau <felipe@illinois.edu>
2016-06-13 18:55:30 +0000

    Addes setup.py, python structure and ups table

---
+++ setup.py
@@ -0,0 +1,19 @@
+import distutils
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*.py") 
+
+# The main call
+setup(name='wavg',
+      version ='0.1',
+      license = "GPL",
+      description = "computes a WAVG[ERR]_MAG_PSF,  SPREAD[ERR]_MODEL quantities for each object in the coadd-catalog.",
+      author = "Brian Yanny",
+      author_email = "yanny@fnal.gov",
+      packages = ['wavg'],
+      package_dir = {'': 'python'},
+      scripts = bin_files,
+      data_files=[('ups',['ups/wavg.table'])],
+      )
+
