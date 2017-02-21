006486edd9ba08f753e80e4b661a29b82e92aad6
yanny <yanny>
2017-02-21 20:30:30 +0000

    fix another RA=360/0 wraparound bug in object-coadd_object matching

--- bin/computewavg.py
+++ bin/computewavg.py
@@ -51,15 +51,10 @@ def readcoaddcat(filename,elems,hduforcoaddcat,urall,uraur,udecll,udecur):
 
   #don't forget about the RA> 350 -360 wraparound
 
-  if cd['ALPHAWIN_J2000'][cindex[0]] < 10 and cd['ALPHAWIN_J2000'][cindex[len(cindex)-1]] > 350:
-        for ii in range(0,len(cd['RA'])):
-	  if cd['ALPHAWIN_J2000'][ii] >= 350:
-		cd['RA'][ii] = cd['ALPHAWIN_J2000'][ii] - 360.0
             
-	#this syntax doesn't work for some reason
-	#cd['RA'] = cd['ALPHAWIN_J2000'] if cd['ALPHAWIN_J2000'] < 350 else cd['ALPHAWIN_J2000'] - 360.0
-
+  coaddcrossra0 = 0
   if (float(urall) > -1 and float(urall) > 350 and float(uraur) < 10):
+    coaddcrossra0 = 1
     uurall = float(urall)-360.0
     uuraur = float(uraur)
     uudecll = float(udecll)
@@ -69,6 +64,11 @@ def readcoaddcat(filename,elems,hduforcoaddcat,urall,uraur,udecll,udecur):
     uuraur = float(uraur)
     uudecll = float(udecll)
     uudecur = float(udecur)
+
+  if coaddcrossra0 == 1:
+        for ii in range(0,len(cd['RA'])):
+	  if cd['ALPHAWIN_J2000'][ii] >= 350:
+		cd['RA'][ii] = cd['ALPHAWIN_J2000'][ii] - 360.0
 
   if (float(urall) > -1):
    lll=len(cd['RA'])
@@ -159,11 +159,13 @@ def readlists(lists,sublistdirprefix,elems,hdrelems,urall,uraur,udecll,udecur):
           nra = numpy.array(od['ALPHAWIN_J2000'])
 	  oindex = nra.argsort()
 
+	  crossra0 = 0
 	  if (float(urall) > -1 and float(urall) > 350 and float(uraur) < 10):
             uurall = float(urall)-360.0
             uuraur = float(uraur)
             uudecll = float(udecll)
 	    uudecur = float(udecur)
+            crossra0 = 1
           else:
             uurall = float(urall)
             uuraur = float(uraur)
@@ -171,7 +173,7 @@ def readlists(lists,sublistdirprefix,elems,hdrelems,urall,uraur,udecll,udecur):
 	    uudecur = float(udecur)
 
 	  #don't forget about the RA-360 for RA>350 wraparound.
-  	  if od['ALPHAWIN_J2000'][oindex[0]] < 10 and od['ALPHAWIN_J2000'][oindex[len(oindex)-1]] > 350:
+  	  if crossra0 == 1:
              for ii in range(0,len(od['RA'])):
 	       if od['ALPHAWIN_J2000'][ii] >= 350:
 		 od['RA'][ii] = od['ALPHAWIN_J2000'][ii] - 360.0
