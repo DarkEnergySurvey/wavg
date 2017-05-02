c606e3b9b0a189c2205472f8ae455a66565ceebf
Felipe Menanteau <felipe@illinois.edu>
2017-05-02 15:37:38 +0000

    Tagging trunk as 0.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*.py")
 
 # The main call
 setup(name='wavg',
-      version ='0.6',
+      version ='0.7',
       license = "GPL",
       description = "computes a WAVG[ERR]_MAG_PSF,  SPREAD[ERR]_MODEL quantities for each object in the coadd-catalog.",
       author = "Brian Yanny",
